703ead690bbb0cde64a78cc5bf570da6a7eb8a4f
안효진 <ahj1848@naver.com>
2017-11-20 18:28:55 +0900

    update add function

---
+++ HW01 201702037 안효진 - 4/src/Project04.java
@@ -0,0 +1,29 @@
+
+public class Project04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("얇고 긴 입술 하나로");
+		System.out.println("온 밤하늘 다 물고 가는");
+		System.out.println("검은 물고기 한 마리");
+		System.out.println();
+		System.out.println("외뿔 하나에");
+		System.out.println("온 몸 다 끌려가는 검은 코뿔소 한 마리");
+		System.out.println();
+		System.out.println("가다가 잠시 멈춰선 검정고양이");
+		System.out.println("입에 물린");
+		System.out.println("생선처럼 파닥이는");
+		System.out.println("은색 나뭇잎 한 장");
+		System.out.println();
+		System.out.println("검정 그물코마다 귀 잡힌 별빛들");
+		System.out.println();
+		System.out.println("나도 당신이라는 깜깜한 세계를");
+		System.out.println("그렇게 다 물어가고 싶다.");
+		System.out.println();
+		System.out.println("- 김경미 -");
+
+
+	}
+
+}
